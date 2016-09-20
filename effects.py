7f581cf08a23e792e5b6b82bd7faecd07e55d3f6
Dave Hocker <athomex10@gmail.com>
2016-09-20 11:32:36 -0500

    Establish fork of Adafruit DotStar library. Begin tracking work on dotstar LED strip.

---
+++ effects.py
@@ -0,0 +1,113 @@
+# Extracted from
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+import time
+
+from dotstar import Adafruit_DotStar
+
+def clear_all(strip):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, 0)
+    strip.show()
+
+def Color(red, green, blue, white = 0):
+    """Convert the provided red, green, blue color to a 24-bit color value.
+    Each color component should be a value 0-255 where 0 is the lowest intensity
+    and 255 is the highest intensity.
+    """
+    return (white << 24) | (red << 16)| (green << 8) | blue
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    print "colorWipe"
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+    """Movie theater light style chaser animation."""
+    print "theaterChase"
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, color)
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+def rainbow(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    print "rainbow"
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i+j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def rainbowCycle(strip, wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    print "rainbowCycle"
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel(((i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def theaterChaseRainbow(strip, wait_ms=50):
+    """Rainbow movie theater light style chaser animation."""
+    print "theaterChaseRainbow"
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, wheel((i+j) % 255))
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+
+
+# Run all effects
+def run_all_effects(strip):
+    # Create NeoPixel object with appropriate configuration.
+    
+    try:
+        print "Effects cycle begins..."
+        # Color wipe animations.
+        colorWipe(strip, Color(255, 0, 0))  # Red wipe
+        colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+        colorWipe(strip, Color(0, 0, 255))  # Green wipe
+        # Theater chase animations.
+        theaterChase(strip, Color(127, 127, 127))  # White theater chase
+        theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+        theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+        # Rainbow animations.
+        rainbow(strip)
+        rainbowCycle(strip)
+        theaterChaseRainbow(strip)
+        
+        print "Effects cycle ends"
+    except KeyboardInterrupt as ex:
+        print ""
+        print "Cycle terminated"
+        raise ex
+    except Exception as ex:
+        print ""
+        print ex
+        raise ex
--- strandtest.py
+++ strandtest.py
@@ -8,45 +8,151 @@
 # USB.  DON'T try that with other code!
 
 import time
+import random
+from collections import deque
 from dotstar import Adafruit_DotStar
+from effects import *
 
-numpixels = 30 # Number of LEDs in strip
+"""
+These are the methods that are available on the Adafruit_DotStar
+strip object. This doc was taken from the dotsar.c source code.
+  { "begin"        , (PyCFunction)begin        , METH_NOARGS , NULL },
+  { "clear"        , (PyCFunction)clear        , METH_NOARGS , NULL },
+  { "setBrightness", (PyCFunction)setBrightness, METH_VARARGS, NULL },
+  { "setPixelColor", (PyCFunction)setPixelColor, METH_VARARGS, NULL },
+  { "show"         , (PyCFunction)show         , METH_VARARGS, NULL },
+  { "Color"        , (PyCFunction)Color        , METH_VARARGS, NULL },
+  { "getPixelColor", (PyCFunction)getPixelColor, METH_VARARGS, NULL },
+  { "numPixels"    , (PyCFunction)numPixels    , METH_NOARGS , NULL },
+  { "getBrightness", (PyCFunction)getBrightness, METH_NOARGS , NULL },
+  { "getPixels"    , (PyCFunction)getPixels    , METH_NOARGS , NULL },
+  { "close"        , (PyCFunction)_close       , METH_NOARGS , NULL },
+"""
+
+numpixels = 30 # Number of LEDs in strip under test
+delay_time = 0.020 # 20 ms delay between writing pixels
+brightness = 16 # 1/8 duty cycle for lower power
 
 # Here's how to control the strip from any two GPIO pins:
-datapin   = 23
-clockpin  = 24
-strip     = Adafruit_DotStar(numpixels, datapin, clockpin)
+# datapin   = 23
+# clockpin  = 24
+# strip     = Adafruit_DotStar(numpixels, datapin, clockpin)
 
-# Alternate ways of declaring strip:
-# strip   = Adafruit_DotStar(numpixels)           # Use SPI (pins 10=MOSI, 11=SCLK)
-# strip   = Adafruit_DotStar(numpixels, 32000000) # SPI @ ~32 MHz
-# strip   = Adafruit_DotStar()                    # SPI, No pixel buffer
-# strip   = Adafruit_DotStar(32000000)            # 32 MHz SPI, no pixel buf
-# See image-pov.py for explanation of no-pixel-buffer use.
-# Append "order='gbr'" to declaration for proper colors w/older DotStar strips)
+# For SPI
+datapin = 10    # GPIO/BCM 10 SPI_MOSI
+clockpin = 11   # GPIO/BCM 11 SPI_SCLK
 
-strip.begin()           # Initialize pins for output
-strip.setBrightness(64) # Limit brightness to ~1/4 duty cycle
+def scroll_pixels(strip, iterations):
+    # Runs 10 LEDs at a time along strip, cycling through red, green and blue.
+    # This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
+    
+    head  = 0               # Index of first 'on' pixel
+    # tail  = -10             # Index of last 'off' pixel
+    tail = -5
+    starting_color = 0xFF0000
+    color_shift = 8
+    # color = 0xFF0000        # 'On' color (starts red)
+    color = starting_color
+    
+    for i in range(iterations): # Loop for number of iterations
+    
+        try:
+            strip.setPixelColor(head, color) # Turn on 'head' pixel
+            if tail >= 0:
+                strip.setPixelColor(tail, 0)     # Turn off 'tail'
+            strip.show()                     # Refresh strip
+            # time.sleep(1.0 / 50)             # Pause 20 milliseconds (~50 fps)
+            time.sleep(delay_time)           # Pause for delay time
+            
+            head += 1                        # Advance head position
+            if(head >= numpixels):           # Off end of strip?
+                head    = 0              # Reset to start
+                # color >>= 8              # Red->green->blue->black
+                color >>= color_shift                # Red->green->blue->black
+                if(color == 0): 
+                    # color = 0xFF0000 # If black, reset to red
+                    color = starting_color # If black, reset
+    
+            tail += 1                        # Advance tail position
+            if(tail >= numpixels): 
+                tail = 0  # Off end? Reset
+    
+        except KeyboardInterrupt:
+            break
 
-# Runs 10 LEDs at a time along strip, cycling through red, green and blue.
-# This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
+    print "Turning off all lights..."
+    # Not well documented, but this is how you turn
+    # off everything
+    strip.clear()
+    strip.show()
 
-head  = 0               # Index of first 'on' pixel
-tail  = -10             # Index of last 'off' pixel
-color = 0xFF0000        # 'On' color (starts red)
+def get_random_int(max_value=100):
+    return int(random.random() * max_value)
 
-while True:                              # Loop forever
+def get_random_color(brightness=2):
+    r = int(get_random_int(max_value=255) / brightness)
+    g = int(get_random_int(max_value=255) / brightness)
+    b = int(get_random_int(max_value=255) / brightness)
+    return Color(r, g, b)
 
-	strip.setPixelColor(head, color) # Turn on 'head' pixel
-	strip.setPixelColor(tail, 0)     # Turn off 'tail'
-	strip.show()                     # Refresh strip
-	time.sleep(1.0 / 50)             # Pause 20 milliseconds (~50 fps)
+def random_pixels(strip):    
+    pixels = deque()
+    active_size = int(strip.numPixels() / 2)
+    color = Color(255, 0, 0)
 
-	head += 1                        # Advance head position
-	if(head >= numpixels):           # Off end of strip?
-		head    = 0              # Reset to start
-		color >>= 8              # Red->green->blue->black
-		if(color == 0): color = 0xFF0000 # If black, reset to red
+    print "{0} random pixels for 10 sec...".format(strip.numPixels())
+    for i in range(int(10.0 / delay_time)):
+        if len(pixels) >= active_size:
+            p = pixels.pop()
+            strip.setPixelColor(p, 0)
+        p = get_random_int(max_value=strip.numPixels())
+        pixels.appendleft(p)
+        strip.setPixelColor(p, get_random_color())
+        strip.show()
+        time.sleep(delay_time)
+    strip.clear()
 
-	tail += 1                        # Advance tail position
-	if(tail >= numpixels): tail = 0  # Off end? Reset
+def main():
+    
+    # Alternate ways of declaring strip:
+    print "Data pin GPIO/BCM {0}".format(datapin)
+    print "Clock pin GPIO/BCM {0}".format(clockpin)
+    print "Opening LED strip with {0} pixels".format(numpixels)
+    # The default here is SPI at 800 KHz
+    # strip   = Adafruit_DotStar(numpixels)           # Use SPI (pins 10=MOSI, 11=SCLK by default)
+    # This strip uses the specified pins at 800 KHz
+    strip   = Adafruit_DotStar(numpixels, datapin, clockpin, order='gbr') # Use SPI (pins 10=MOSI, 11=SCLK)
+    # strip   = Adafruit_DotStar(numpixels, 32000000) # SPI @ ~32 MHz
+    # strip   = Adafruit_DotStar()                    # SPI, No pixel buffer
+    # strip   = Adafruit_DotStar(32000000)            # 32 MHz SPI, no pixel buf
+    # See image-pov.py for explanation of no-pixel-buffer use.
+    # Append "order='gbr'" to declaration for proper colors w/older DotStar strips)
+    
+    strip.begin()           # Initialize pins for output
+    # strip.setBrightness(64) # Limit brightness to ~1/4 duty cycle
+    strip.setBrightness(brightness) # Limit brightness
+    # Runs 10 LEDs at a time along strip, cycling through red, green and blue.
+    # This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
+    
+    print "Hit Ctrl-C to end test"
+    
+    try:
+        while True:
+            random_pixels(strip)
+            run_all_effects(strip)
+            scroll_pixels(strip, numpixels * 20)
+    except (KeyboardInterrupt, Exception) as ex:
+        print ex
+        print ""
+        print "Turning off all lights..."
+        # Not well documented, but this is how you turn
+        # off everything
+        strip.clear()
+        strip.show()
+
+        strip.close()
+        print "Strip closed"
+    
+    
+if __name__ == "__main__":
+    main()
